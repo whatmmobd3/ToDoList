619ec6c39ec437df61483b9a8c8eb461da43f93d
David Nguyen <david.nguyen@terralogic.com>
2021-11-24 13:57:55 +0700

    Initial Commit

---
+++ Shared/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  Shared
+//
+//  Created by David Nguyen on 24/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
